e656d2b69261dca2f8d13d5bfd20bb0933541bd3
lyleaigbedion <Lyle-Anthony.Aigbedion75@myhunter.cuny.edu>
2020-06-28 11:17:38 -0400

    realized the current version of TensorFlow.js does not have a simplified way to save and export a retrained model, will rewrite in ml5.js for this reason

--- src/index.js
+++ src/index.js
@@ -8,114 +8,122 @@ import { models } from '@tensorflow/tfjs';
 
 
 
-async function loadMobilenet() {
-  return await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json')
-}
-function loadImage(src) {
-  return new Promise((resolve, reject) => {
-  const img = new Image();
-  img.src = src;
-  img.onload = () =>
-    resolve(tf.browser.fromPixels(img));
-    img.onerror = (err) =>
-      reject(err);
-  });
-}
-
-function cropImage(img) {
-  const width = img.shape[0];
-  const height = img.shape[1];  // use the shorter side as the size to which we will crop
-  const shorterSide = Math.min(img.shape[0], img.shape[1]);  // calculate beginning and ending crop points
-  const startingHeight = (height - shorterSide) / 2;  const startingWidth = (width - shorterSide) / 2;  const endingHeight = startingHeight + shorterSide;  const endingWidth = startingWidth + shorterSide;  // return image data cropped to those points
-  return img.slice([startingWidth, startingHeight, 0], [endingWidth, endingHeight, 3]);
-}
-
-function resizeImage(image) {
-  return tf.image.resizeBilinear(image, [224, 224]);
-}
-
-function batchImage(image) {
-  // Expand our tensor to have an additional dimension, whose size is 1
-  const batchedImage = image.expandDims(0);  // Turn pixel data into a float between -1 and 1.
-  return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
-}
-
-function loadAndProcessImage(image) {
-  const croppedImage = cropImage(image);
-  const resizedImage = resizeImage(croppedImage);
-  const batchedImage = batchImage(resizedImage);
-  return batchedImage;
-}
-
-import drum from '../data/pretrained-model-data/drum.jpg';
-loadMobilenet().then(pretrainedModel => {
-    loadImage(drum).then(img => {
-      const processedImage = loadAndProcessImage(img);
-      const prediction = pretrainedModel.predict(processedImage);    // Because of the way Tensorflow.js works, you must call print on a Tensor instead of console.log.
-      prediction.print();
-        });
-});
-
-// const webcamElement = document.getElementById('webcam');
-// let net;
-// const classifier = knnClassifier.create();
-
-
-// async function app() {
-//   console.log('Loading mobilenet..');
-
-//   // Load the model
-//   net = await mobilenet.load();
-//   console.log('Successfully loaded model');
-
-//   // Create an object from Tensorflow.js data API which could capture image
-//   // from the web camera as Tensor.
-//   const webcam = await tf.data.webcam(webcamElement);
-
-//   // Reads an image from the webcam and associates it with a specific class
-//   // index.
-//   const addExample = async classId => {
-//     // Capture an image from the web camera.
-//     const img = await webcam.capture();
-
-//     // Get the intermediate activation of MobileNet 'conv_preds' and pass that
-//     // to the KNN classifier.
-//     const activation = net.infer(img, true);
-
-//     // Pass the intermediate activation to the classifier.
-//     classifier.addExample(activation, classId);
-
-//     // Dispose the tensor to release the memory.
-//     img.dispose();
-//   };
-
-//   // When clicking a button, add an example for that class.
-//   document.getElementById('class-a').addEventListener('click', () => addExample(0));
-//   document.getElementById('class-b').addEventListener('click', () => addExample(1));
-//   document.getElementById('class-c').addEventListener('click', () => addExample(2));
-
-//   while (true) {
-//     if (classifier.getNumClasses() > 0) {
-//       const img = await webcam.capture();
-
-//       // Get the activation from mobilenet from the webcam.
-//       const activation = net.infer(img, 'conv_preds');
-//       // Get the most likely class and confidence from the classifier module.
-//       const result = await classifier.predictClass(activation);
-
-//       const classes = ['Pen', 'Book', 'Wall'];
-//       document.getElementById('console').innerText = `
-//         prediction: ${classes[result.label]}\n
-//         probability: ${result.confidences[result.label]}
-//       `;
-
-//       // Dispose the tensor to release the memory.
-//       img.dispose();
-//     }
-
-//     await tf.nextFrame();
-//   }
+// async function loadMobilenet() {
+//   return await tf.loadLayersModel('https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json')
 // }
+// function loadImage(src) {
+//   console.log(src)
+//   return new Promise((resolve, reject) => {
+//   const img = new Image();
+//   img.src = src;
+//   img.width = 1000;
+//   img.height = 1000;
+//     console.log(img)
+//   img.onload = async () =>
+//   console.log(img)
+
+//      await tf.browser.fromPixels(img);
+//     // img.onerror = (err) =>
+//     //   reject(err);
+//   });
+// }
+
+// function cropImage(img) {
+//   const width = img.shape[0];
+//   const height = img.shape[1];  // use the shorter side as the size to which we will crop
+//   const shorterSide = Math.min(img.shape[0], img.shape[1]);  // calculate beginning and ending crop points
+//   const startingHeight = (height - shorterSide) / 2;  const startingWidth = (width - shorterSide) / 2;  const endingHeight = startingHeight + shorterSide;  const endingWidth = startingWidth + shorterSide;  // return image data cropped to those points
+//   return img.slice([startingWidth, startingHeight, 0], [endingWidth, endingHeight, 3]);
+// }
+
+// function resizeImage(image) {
+//   return tf.image.resizeBilinear(image, [224, 224]);
+// }
+
+// function batchImage(image) {
+//   // Expand our tensor to have an additional dimension, whose size is 1
+//   const batchedImage = image.expandDims(0);  // Turn pixel data into a float between -1 and 1.
+//   return batchedImage.toFloat().div(tf.scalar(127)).sub(tf.scalar(1));
+// }
+
+// function loadAndProcessImage(image) {
+//   const croppedImage = cropImage(image);
+//   const resizedImage = resizeImage(croppedImage);
+//   const batchedImage = batchImage(resizedImage);
+//   return batchedImage;
+// }
+
+// import drum from '../data/pretrained-model-data/pen.jpg';
+// loadMobilenet().then(pretrainedModel => {
+//     loadImage(drum).then(img => {
+//       const processedImage = loadAndProcessImage(img);
+//       const prediction = pretrainedModel.predict(processedImage);    // Because of the way Tensorflow.js works, you must call print on a Tensor instead of console.log.
+//       prediction.as1D().argMax().print();;
+//         });
+// });
+
+const webcamElement = document.getElementById('webcam');
+let net;
+const classifier = knnClassifier.create();
+
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Create an object from Tensorflow.js data API which could capture image
+  // from the web camera as Tensor.
+  const webcam = await tf.data.webcam(webcamElement);
+
+  // Reads an image from the webcam and associates it with a specific class
+  // index.
+  const addExample = async classId => {
+    // Capture an image from the web camera.
+    const img = await webcam.capture();
+
+    // Get the intermediate activation of MobileNet 'conv_preds' and pass that
+    // to the KNN classifier.
+    const activation = net.infer(img, true);
+
+    // Pass the intermediate activation to the classifier.
+    classifier.addExample(activation, classId);
+
+    // Dispose the tensor to release the memory.
+    img.dispose();
+  };
+
+  // When clicking a button, add an example for that class.
+  document.getElementById('Paper_Mario').addEventListener('click', () => addExample(0));
+  document.getElementById('Pokemon_Stadium_2').addEventListener('click', () => addExample(1));
+  document.getElementById('Mario_64').addEventListener('click', () => addExample(2));
+  document.getElementById('class-null').addEventListener('click', () => addExample(3));
+  document.getElementById('save-model').addEventListener('click', async () =>{await   ('downloads://my-model')} )
+
+  while (true) {
+    if (classifier.getNumClasses() > 0) {
+      const img = await webcam.capture();
+
+      // Get the activation from mobilenet from the webcam.
+      const activation = net.infer(img, 'conv_preds');
+      // Get the most likely class and confidence from the classifier module.
+      const result = await classifier.predictClass(activation);
+
+      const classes = ['Paper Mario', 'Pokemon Stadium 2', 'Mario 64', 'Not A VideoGame'];
+      document.getElementById('console').innerText = `
+        prediction: ${classes[result.label]}\n
+        probability: ${result.confidences[result.label]}
+      `;
+
+      // Dispose the tensor to release the memory.
+      img.dispose();
+    }
+
+    await tf.nextFrame();
+  }
+}
 
 
 
@@ -185,7 +193,7 @@ loadMobilenet().then(pretrainedModel => {
 
 // // }
 
-// app();
+app();
 // console.log('Testing?')
 
 
